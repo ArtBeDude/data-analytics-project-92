c498d0b9bd49f7d30d5e770e9cdb306a61f895d9
ArtBeDude <ArturShavlinsky01@gmail.com>
2024-09-09 08:54:36 +0300

    queries.sql

--- queries.sql
+++ queries.sql
@@ -206,7 +206,7 @@ on tab4.selling_month = tab3.selling_month
 
 /* В этом запросе мы будем искать даты первых акционных покупок
  * клиентами.
- * **special_offer**
+ * __special_offer__
 */
 
 with tab1 as (
