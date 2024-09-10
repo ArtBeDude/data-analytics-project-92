10ab77255eef2298a3ff0c49d5d246c5bd46db82
ArtBeDude <ArturShavlinsky01@gmail.com>
2024-09-10 13:54:13 +0300

    Update queries.sql
    without com

--- queries.sql
+++ queries.sql
@@ -4,11 +4,10 @@
 select
 	count(customer_id) as customers_count
 from customers
-/* Вычисление количества клиентов из таблицы "сustomers"*/
 
 --------------------------------------------------------------
 	
-/* Request to find the top 10 sellers with the highest amounts
+/* Query to find the top 10 sellers with the highest amounts
 sales
 * top_10_total_income
 */
@@ -31,22 +30,17 @@ left join
  * "employees" tables
 */
 select
-	distinct seller, -- Выбор уникальных продавцов
+	distinct seller, -- Choose only unique sellers
 	count(*) 
-		over (partition by seller) as operations, -- Вычисление количества операций одного продавца
+		over (partition by seller) as operations, 
 	floor(sum(quantity * price) 
-		over (partition by seller)) as income -- Вычисление суммы продаж продавца и округление до целого
+		over (partition by seller)) as income 
 from tab1
 order by income desc
 limit 10
-/* В итоговом запросе мы получили таблицу с 10-ю продавцами
-   с самыми большими суммами продаж. */
 
 --------------------------------------------------------------	
 
-/* Запрос по поиску худших продавцов по средней сумме продаж
-* lowest_average_income
-*/	
 with tab1 as (
 select 
 	concat(e.first_name,' ',e.last_name) as seller,
@@ -61,25 +55,18 @@ left join
 	products p 
 	on s.product_id = p.product_id
 ),
-/* В подзапросе "tab1" мы соединяем таблицы согласно референсам их id.
- * Выбираем необходимые столбцы, объеденяем столбцы "e.first_name" и "e.last_name" из
- * таблицы "employees"
- */
 tab2 as (
 select
-	 distinct seller, -- оставляем только уникальные имена продавцов
+	 distinct seller, 
 	floor(AVG(quantity * price) 
-		over (partition by seller)) as average_income, -- Вычисляем среднюю сумму продажи по продавцу
+		over (partition by seller)) as average_income, 
 	AVG(quantity * price) 
-		over () as avg_total -- Вычисляем среднюю сумму по всем продажам
+		over () as avg_total 
 from tab1
 )
-/* В подзапросе tab2 мы вычисляем значение средних продаж 
- * для последюущего их сравнения.
- */
 select
 	seller,
-	average_income as average_income -- округляем значение до целого числа
+	average_income as average_income 
 from tab2
 group by 1,2
 having average_income < AVG(avg_total)
@@ -87,17 +74,14 @@ order by average_income
 	
 --------------------------------------------------------------
 
-/* Запрос по поиску продаж продавцов в разрезе дней недели.
- * day_of_week_income
- */
 with tab1 as (
 select 
 	concat(e.first_name,' ',e.last_name) as seller,
 	p.product_id,
 	s.quantity,
 	p.price,
-	(EXTRACT(ISODOW FROM s.sale_date) - 1) as num_of_day, -- приводим нумерацию к Mon = 0
-	to_char(s.sale_date, 'Day') as day_of_week -- выделяем название дня недели
+	(EXTRACT(ISODOW FROM s.sale_date) - 1) as num_of_day, 
+	to_char(s.sale_date, 'Day') as day_of_week 
 from sales s
 left join 
 	employees e 
@@ -106,10 +90,6 @@ left join
 	products p 
 	on s.product_id = p.product_id
 ),
-/* В подзапросе tab2 мы выводим уникальных продавцов и
- * считаем сумму продаж каждого продавца в партиции продавца и дня недели
- */ 
-
 tab2 as (
 select
 	distinct seller,
@@ -126,19 +106,12 @@ select
 	floor(income) as income
 from tab2
 
-/* Итоговая витрина продаж в разрезе продавцов и дней
-*/
-
 --------------------------------------------------------------
 
-/* Запрос по вычислению количества покупателей в разрезе
- * возрастных групп.
- * age_groups
-*/
 with tab1 as ()
 select 
 	*,
-	case -- Присвоение категорий каждому диапазону возрастов
+	case -- 
 		when age between 16 and 25 then '16-25'
 		when age between 26 and 40 then '26-40'
 		when age > 40 then '40+'
@@ -147,18 +120,14 @@ from customers c
 )
 select 
 	distinct age_category,
-	count(age) over (partition by age_category) as age_count -- подсчёт количества покупателей в категории
+	count(age) over (partition by age_category) as age_count 
 from tab1
 order by age_category;
 
 --------------------------------------------------------------
 
-/* В этом подзапросе мы вычисляем количество уникальных покупателей
- * и выручки в разрезе каждого месяца.
- * customers_by_month
- */
-with tab1 as ( -- В подзапросе tab1 происходит приведение данных
-select	       -- к необходимым типам.
+with tab1 as ( 
+select	       
 	to_char(s.sale_date, 'YYYY-MM') as selling_month,
 	concat(c.first_name,' ',c.last_name) as customer_name
 from sales s
@@ -166,20 +135,20 @@ left join
 	customers c 
 	on s.customer_id = c.customer_id
 ),
-tab2 as (     -- В подзапросе tab2 мы находим уникальных покупателей в каждом месяце.
+tab2 as (     
 select distinct customer_name,
 selling_month
 from tab1
 ),
-tab3 as (     -- В подзапросе tab3 мы находим количество уникальных покупателей
-select	      -- в разрезе каждого месяца.
+tab3 as (     
+select	      
 	distinct selling_month,
 	count(customer_name)
 		over (partition by selling_month)
 from tab2
 ),
-tab4 as (     -- tab4 представляет собой CTE с данными для подсчета суммарной выручки
-select        -- в итоговом запросе.
+tab4 as (     
+select        
 	to_char(s.sale_date, 'YYYY-MM') as selling_month,
 	p.product_id,
 	s.quantity,
@@ -197,30 +166,21 @@ select
 from tab4
 inner join tab3
 on tab4.selling_month = tab3.selling_month
-/* В итоговом запросе мы соеденили CTE tab4 и tab3 по месяцам,
- * посчитали и округлили суммарную выручку по месяцам и указали количество уникальных клиентов
- * в каждом месяце
- */
 
 --------------------------------------------------------------
-
-/* В этом запросе мы будем искать даты первых акционных покупок
- * клиентами.
- * special_offer
-*/
 
 with tab1 as (
 select		   
 	s.customer_id,
-	concat(c.first_name,' ',c.last_name) as customer, - объеденяем имя и фамилию
+	concat(c.first_name,' ',c.last_name) as customer, 
 	sale_date,
 	concat(e.first_name,' ',e.last_name) as seller,
 	p.price,
-	row_number () -- присваиваем номера в партиции клиент, продавец с сортировкой по дате ASC
+	row_number () 
 		over (partition by concat(c.first_name,' ',c.last_name),  
 			concat(e.first_name,' ',e.last_name) 
 			order by sale_date) as flag_1,
-	row_number () -- присваиваем номер каждой записи с клиентом для последующего отбора первого значения
+	row_number () 
 		over (partition by concat(c.first_name,' ',c.last_name)) as flag_2 
 from sales s
 left join 
@@ -232,18 +192,16 @@ left join
 left join 
 	products p 
 	on s.product_id = p.product_id
-where price = 0 - условие выбора записей соответствующее акции
+where price = 0 
 )
 select
 	customer,
 	sale_date,
 	seller
 from tab1 
-where flag_1 = 1 and flag_2 = 1 -- выбор flag_1 = первая клиент-дата, flag_2 = одна запись - один клиент
-order by customer_id -- сортировка записей по id клиента
+where flag_1 = 1 and flag_2 = 1
+order by customer_id
 
-/* Итоговая таблица предоставляет даты первых покупок клиентами соответствующих условиям акции
- *
 
 
 
